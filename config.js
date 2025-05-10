cee118de012fd47f92bf17efee20e4bcdaa39a64
Yubo Sun <53847419+pikachu233666@users.noreply.github.com>
2025-05-10 16:55:49 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,19 +1,33 @@
 window.Config = {
 
-  SiteName: 'Judgeflow Public Status',
+  // 显示标题
+  SiteName: 'Public Status',
 
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2936902-bbf47bc466f5d3c0ae0b56b2',
+    'ur1900384-f828e4054bd24c1b399ff9be'
   ],
 
+  // 日志天数
   CountDays: 90,
 
+  // 是否显示检测站点的链接
   ShowLink: true,
 
+  // 导航栏菜单
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://judgeflow.com/'
+      url: 'https://status.pochacco.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/pikachu233666/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://pochacco.top/'
     },
   ],
 };
