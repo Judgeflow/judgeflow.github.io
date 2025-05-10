cd739cc3c1c557095b80e6707ec92ddf6f2cd6e3
Yubo Sun <53847419+pikachu233666@users.noreply.github.com>
2025-05-10 16:43:41 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+window.Config = {
+
+  SiteName: 'Judgeflow Public Status',
+
+  ApiKeys: [
+    'ur2936902-bbf47bc466f5d3c0ae0b56b2',
+  ],
+
+  CountDays: 90,
+
+  ShowLink: true,
+
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://judgeflow.com/'
+    },
+  ],
+};
